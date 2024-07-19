0aedea0a5c4e92027b333a791b59296a6073e1c6
Diogo <diogo322008@hotmail.com>
2024-07-19 00:25:20 -0300

    criando ec2 via terraform, e adicionando arquivo state do terraform no s3, um bucket do s3 é utilizado como backend do terraform

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.0"
-}
-
 terraform {
-  backend "s3" {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket = "descomplicando-terraform-gomex-tfstates"
+    bucket = "descomplicando-terraform-diogo-tfstates"
     key    = "terraform-test.tfstate"
     region = "us-east-1"
   }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
