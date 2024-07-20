006f9cd8b4cc82c879abe2ae2f62f6671aeede02
Diogo <diogo322008@hotmail.com>
2024-07-20 18:50:26 -0300

    update melhores praticas version.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
    backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
     bucket = "descomplicando-terraform-diogo-tfstates"
